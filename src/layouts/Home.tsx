2fde6c7097fc3f166403898fe34a420f6b6c630e
Alaa <alaaali@meltwater.org>
2023-02-18 22:55:59 +0200

    first commit

---
+++ src/layouts/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../components/header"
+
+export default class Home extends Component {
+  render(): React.ReactNode {
+    
+    return (
+      <>
+      <Header />
+      <h1 style={{ marginTop: '3%' }}>hi</h1></> 
+    )
+  }
+}

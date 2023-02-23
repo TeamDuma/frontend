cdf242c79748638c458efd0fcd6700d22e94f3a9
Alaa <alaaali@meltwater.org>
2023-02-23 22:54:45 +0200

    modify files

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,84 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+// import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import "./Header.css";
+import { ListItemText } from "@mui/material";
 
-const Header = () => (
-  <div className="header">
-    <h1> hi</h1>
-  </div>
-)
+const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+    },
+    {
+      text: "Products",
+    },
+    {
+      text: "Shops",
+    },
+    {
+      text: "About",
+    },
+    {
+      text: "Contact",
+    },
+  ];
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <div className="navbar-links-container">
+          <a href="/">
+            <h5 style={{ fontSize: "30px", width: "600%", marginLeft: "25%" }}>
+              Duma
+            </h5>
+          </a>
+        </div>
+      </div>
+      <div className="navbar-links-container">
+        <a href="/">Home</a>
+        <a href="/Products">Products</a>
+        <a href="/Shop">Shops</a>
+        <a href="/About">About</a>
+        <a href="/Contact">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  {/* <ListItemIcon>{item.icon}</ListItemIcon> */}
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </nav>
+  );
+};
+
 export default Header;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from '../layouts/Home';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Home from '../components/Home/Home';
+import Products from '../components/Products';
+import Shop from '../components/Shop';
+
+
+
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/Products' element={<Products />} />
+       <Route path='/About' element={<About />} />
+       <Route path='/Shop' element={<Shop />} /> 
+       <Route path='/Contact' element={<Contact />} />
     </Routes>
   );
 }

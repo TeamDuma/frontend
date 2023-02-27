e1e8e800ee34363b66548083641c3ebc431b867d
Alaa <alaaali@meltwater.org>
2023-02-27 11:43:09 +0200

    fixed the path to the directories

--- src/components/Home/HowItworks.tsx
+++ src/components/Home/HowItworks.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-import img1 from "/Users/a/Desktop/Capstone_two/setup/src/Assets/step1.png";
-import img2 from "/Users/a/Desktop/Capstone_two/setup/src/Assets/step2.png";
-import img3 from "/Users/a/Desktop/Capstone_two/setup/src/Assets/step3.png";
+import img1 from "../../Assets/step1.png";
+import img2 from "../../Assets/step2.png";
+import img3 from "../../Assets/step3.png";
 
 const HowItworks = () => {
   return (
@@ -14,61 +14,125 @@ const HowItworks = () => {
       </div>
       <Row>
         <Col>
-
-        <Card className="bg-dark text-white" style={{margin:'3%',color:'#808080'}}>
-        <Card.Text style={{margin:'3%',color:'#808080'}}>
-          step1
-        </Card.Text>
-        <Card.Title style={{marginBottom:'1%',fontSize:'50px',color: '#F58929',marginLeft:'3%'}}>Browse through our <br/>hot deals</Card.Title>
-        <Card.Text style={{margin:'3%',color:'#404040'}}>
-        We have partnered with trusted retailers to get the best deals in town on our platform.<br/>
-Browse through their offers shop by shop and find deals near you.
-Each product contains details on nutritional value, sell-by date and number of <br/>products remaining at that offer. Hurry while stocks last!
-        </Card.Text>
-          </Card>     
-             </Col>
+          <Card
+            className="bg-dark text-white"
+            style={{ margin: "3%", color: "#808080" }}
+          >
+            <Card.Text style={{ margin: "3%", color: "#808080" }}>
+              step1
+            </Card.Text>
+            <Card.Title
+              style={{
+                marginBottom: "1%",
+                fontSize: "50px",
+                color: "#F58929",
+                marginLeft: "3%",
+              }}
+            >
+              Browse through our <br />
+              hot deals
+            </Card.Title>
+            <Card.Text style={{ margin: "3%", color: "#404040" }}>
+              We have partnered with trusted retailers to get the best deals in
+              town on our platform.
+              <br />
+              Browse through their offers shop by shop and find deals near you.
+              Each product contains details on nutritional value, sell-by date
+              and number of <br />
+              products remaining at that offer. Hurry while stocks last!
+            </Card.Text>
+          </Card>
+        </Col>
         <Col>
-        <Card className="bg-dark text-white">
-        <Card.Img src={img1} alt="Card image" style={{height:'100%',width:'50%',marginLeft:'20%',marginTop:'5%'}} />
-          </Card>   
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src={img1}
+              alt="Card image"
+              style={{
+                height: "100%",
+                width: "50%",
+                marginLeft: "20%",
+                marginTop: "5%",
+              }}
+            />
+          </Card>
         </Col>
       </Row>
       <Row>
-      <Col>
-        <Card className="bg-dark text-white">
-        <Card.Img src={img2} alt="Card image" style={{height:'100%',width:'50%',marginLeft:'30%'}} />
-          </Card>   
+        <Col>
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src={img2}
+              alt="Card image"
+              style={{ height: "100%", width: "50%", marginLeft: "30%" }}
+            />
+          </Card>
         </Col>
         <Col>
-
-<Card className="bg-dark text-white" style={{margin:'3%',color:'#808080'}}>
-<Card.Text style={{margin:'3%',color:'#808080'}}>
-  step2
-</Card.Text>
-<Card.Title style={{marginBottom:'1%',fontSize:'50px',color: '#F58929',marginLeft:'3%'}}>Verify your purchase and pay</Card.Title>
-<Card.Text style={{margin:'3%',color:'#404040'}}>
-After you have finished selecting your products, check your cart to make sure you have collected all you need. You will then be prompted to pay using your preferred payment method.</Card.Text>
-  </Card>     
-     </Col>
-      
+          <Card
+            className="bg-dark text-white"
+            style={{ margin: "3%", color: "#808080" }}
+          >
+            <Card.Text style={{ margin: "3%", color: "#808080" }}>
+              step2
+            </Card.Text>
+            <Card.Title
+              style={{
+                marginBottom: "1%",
+                fontSize: "50px",
+                color: "#F58929",
+                marginLeft: "3%",
+              }}
+            >
+              Verify your purchase and pay
+            </Card.Title>
+            <Card.Text style={{ margin: "3%", color: "#404040" }}>
+              After you have finished selecting your products, check your cart
+              to make sure you have collected all you need. You will then be
+              prompted to pay using your preferred payment method.
+            </Card.Text>
+          </Card>
+        </Col>
       </Row>
       <Row>
         <Col>
-
-        <Card className="bg-dark text-white" style={{margin:'3%',color:'#808080'}}>
-        <Card.Text style={{margin:'3%',color:'#808080'}}>
-          step3
-        </Card.Text>
-        <Card.Title style={{marginBottom:'1%',fontSize:'50px',color: '#F58929',marginLeft:'3%'}}>Choose your delivery option</Card.Title>
-        <Card.Text style={{margin:'3%',color:'#404040'}}>
-        We aim to connect you to shops nearest to you, so you have the option of using <br/> our courier partner, or passing by the shop and collecting your shopping within the same day you purchase.
-        </Card.Text>
-          </Card>     
-             </Col>
+          <Card
+            className="bg-dark text-white"
+            style={{ margin: "3%", color: "#808080" }}
+          >
+            <Card.Text style={{ margin: "3%", color: "#808080" }}>
+              step3
+            </Card.Text>
+            <Card.Title
+              style={{
+                marginBottom: "1%",
+                fontSize: "50px",
+                color: "#F58929",
+                marginLeft: "3%",
+              }}
+            >
+              Choose your delivery option
+            </Card.Title>
+            <Card.Text style={{ margin: "3%", color: "#404040" }}>
+              We aim to connect you to shops nearest to you, so you have the
+              option of using <br /> our courier partner, or passing by the shop
+              and collecting your shopping within the same day you purchase.
+            </Card.Text>
+          </Card>
+        </Col>
         <Col>
-        <Card className="bg-dark text-white">
-        <Card.Img src={img3} alt="Card image" style={{height:'100%',width:'50%',marginLeft:'20%',marginTop:'5%'}} />
-          </Card>   
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src={img3}
+              alt="Card image"
+              style={{
+                height: "100%",
+                width: "50%",
+                marginLeft: "20%",
+                marginTop: "5%",
+              }}
+            />
+          </Card>
         </Col>
       </Row>
       {/* <Card className="bg-dark text-white">
